614485344f09941b353970787178dc7a9d90131c
JMC-Academy <bvotano@myjmc.edu.au>
2024-02-22 11:27:47 +1100

    Pick Ups Implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,11 +6,16 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 1f;
     private Rigidbody rb;
+    private int pickUpCount;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        //Get the number of pick ups in our scene
+        pickUpCount = GameObject.FindGameObjectsWithTag("Pick Up").Length;
+        //Run the Check Pickups Function
+        CheckPickUps();
     }
     
     // Update is called once per frame
@@ -23,4 +28,25 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movement * speed);
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Pick Up")
+        {
+            //Destroy the collided object
+            Destroy(other.gameObject);
+            //Decrement the pick up count
+            pickUpCount--;
+            //Run the Check Pick Ups function
+            CheckPickUps();
+        }
+    }
+
+    void CheckPickUps()
+    {
+        print("Pick Ups Left: " + pickUpCount);
+        if(pickUpCount == 0)
+        {
+            print("Yay! You Won");
+        }
+    }
 }
